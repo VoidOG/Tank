a5e69856aaf930da0bcd619f9791e46f65696e2f
VoidOG <VoidGod123@outlook.com>
2024-10-15 16:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = "7804893878:AAGWRLFNDt21PiQtkAhapvUmd-2YQZjYHJw"
+TELEGRAM_BOT_TOKEN = '7804893878:AAGWRLFNDt21PiQtkAhapvUmd-2YQZjYHJw'
+LOG_GROUP_ID = -1002035333875
 
 def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    username = user.username if user.username else "No username"
+    user_id = user.id
+    name = user.full_name
+    is_dm = "Yes" if update.effective_chat.type == "private" else "No"
+    
+    log_message = f"User started the bot:\nUsername: {username}\nUser ID: {user_id}\nName: {name}\nIn DM: {is_dm}"
+    
+    context.bot.send_message(chat_id=LOG_GROUP_ID, text=log_message)
+
     image_url = "https://i.ibb.co/kqmt7bk/file-101.jpg"
     
     caption = """
@@ -11,7 +22,6 @@ Welcome to the Tanks Arena Bot! Click the button below to play:
 
 - Enjoy thrilling multiplayer tank battles.
 - Compete with players from all around the world.
-
 """
 
     keyboard = [
@@ -27,18 +37,12 @@ Welcome to the Tanks Arena Bot! Click the button below to play:
     )
 
 def main() -> None:
-    # Create the Updater and pass it your bot's token
-    updater = Updater(TELEGRAM_BOT_TOKEN)
-    
+    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-    
-    # Register the /start command handler
     dispatcher.add_handler(CommandHandler("start", start))
     
-    # Start the Bot
     updater.start_polling()
     
-    # Run the bot until you send a signal to stop it
     updater.idle()
 
 if __name__ == '__main__':
